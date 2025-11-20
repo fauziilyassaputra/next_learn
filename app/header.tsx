2a4ecacedc3521b31af0e85af8f902dfcb15d15c
fauzi <fauziilyassaputra@gmail.com>
2025-11-20 13:55:19 +0700

    create server action

--- app/header.tsx
+++ app/header.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+// import Link from "next/link";
 import { getSetting } from "./queries/getSetting";
 import { Nav } from "./nav";
 import { Suspense } from "react";
--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import { post } from "@/app/_types/post";
+import { createComment } from "@/app/actions";
 
 async function getPost(slug: string): Promise<post> {
   // http://localhost:3001/post?slug=post-1 => maka hanya objek pertama yang akan diambil
@@ -16,9 +17,23 @@ export default async function PostsPage({
   const { slug } = await params;
   const post = await getPost(slug);
   return (
-    <article>
-      <h1>{post.title}</h1>
-      <div className="">{post.content}</div>
-    </article>
+    <>
+      <article>
+        <h1 className="text-lg">{post.title}</h1>
+        <div className="">{post.content}</div>
+      </article>
+      <section className="mt-4">
+        <h2>Comments</h2>
+        <form action={createComment} className="flex flex-col w-72">
+          <textarea
+            name="comment"
+            id=""
+            placeholder="write your comment..."
+            className="border-2 border-black w-72 h-40 p-2"
+          ></textarea>
+          <button className="bg-blue-400">Send</button>
+        </form>
+      </section>
+    </>
   );
 }
